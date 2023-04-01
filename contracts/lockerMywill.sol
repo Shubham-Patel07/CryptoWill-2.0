ee8f96f861fc26d4447ba9de37eb2159d6a1cd97
Shubham-Patel07 <shubham.patel.btech2020@sitpune.edu.in>
2023-04-01 15:10:37 +0530

    Add: lockerMyWill.sol

---
+++ contracts/lockerMywill.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+import "./Mywill.sol";
+
+contract lockerMyWill {
+    mapping(address => address[]) public lockers;
+
+    // get all Lockers associated with given address
+    function getLocker(address _owner) public view returns (address[] memory) {
+        return lockers[_owner];
+    }
+
+    event NewLocker(address locker, address owner);
+
+    // create new locker
+    function newLocker(
+        string memory _name,
+        address _owner,
+        uint256 _lockingTime,
+        address _beneficiary
+    ) external {
+        // create new locker
+        MyWill locker = new MyWill(_name, _owner, _lockingTime, _beneficiary);
+
+        // Add mapping
+        lockers[_owner].push(address(locker));
+
+        // emit new locker event
+        emit NewLocker(address(locker), _owner);
+    }
+}
